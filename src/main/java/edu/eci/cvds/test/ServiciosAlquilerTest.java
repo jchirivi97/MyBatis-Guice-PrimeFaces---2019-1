9e3a0493ca2f39fb0397a6deb726580b39451ce6
jimmy chirivi <jimmychirivi09@gmail.com>
2019-03-20 19:03:42 -0500

    final

---
+++ src/main/java/edu/eci/cvds/test/ServiciosAlquilerTest.java
@@ -0,0 +1,68 @@
+package edu.eci.cvds.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.inject.Inject;
+import edu.eci.cvds.sampleprj.dao.PersistenceException;
+import edu.eci.cvds.samples.entities.Cliente;
+import edu.eci.cvds.samples.entities.Item;
+import edu.eci.cvds.samples.entities.ItemRentado;
+import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.cvds.samples.services.ServiciosAlquiler;
+import edu.eci.cvds.samples.services.ServiciosAlquilerFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.quicktheories.core.Gen;
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.Generate.*;
+import static org.quicktheories.generators.SourceDSL.*;
+
+public class ServiciosAlquilerTest {
+
+    @Inject
+    private SqlSession sqlSession;
+
+    ServiciosAlquiler serviciosAlquiler;
+
+    public ServiciosAlquilerTest() {
+        serviciosAlquiler = ServiciosAlquilerFactory.getInstance().getServiciosAlquilerTesting();
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void emptyDB() {
+        qt().forAll(longs().from(1).upTo(1000)).check((documento) -> {
+            boolean r = false;
+            try {
+                Cliente cliente = serviciosAlquiler.consultarCliente(documento);
+            } catch(ExcepcionServiciosAlquiler e) {
+                r = true;
+            } catch(IndexOutOfBoundsException e) {
+                r = true;
+            }
+            return r;
+        });
+        
+        
+        qt().forAll(integers().between(1, 5000)).check((iditem) -> {
+            boolean i = false;
+            try {
+                Item item = serviciosAlquiler.consultarItem(iditem);
+            } catch(ExcepcionServiciosAlquiler e) {
+                i = true;
+            } catch(IndexOutOfBoundsException e) {
+                i = true;
+            }
+            return i;
+        });
+        
+        
+    }
+}
